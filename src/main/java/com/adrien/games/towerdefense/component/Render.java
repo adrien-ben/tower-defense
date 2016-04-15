50995da031313901f0eec04125d43687cecdd60c
Adrien Bennadji <adrien-ben@users.noreply.github.com>
2016-04-15 15:55:29 +0200

    Removed shape from Render component

--- src/main/java/com/adrien/games/towerdefense/component/Render.java
+++ src/main/java/com/adrien/games/towerdefense/component/Render.java
@@ -6,20 +6,10 @@ import java.awt.*;
 
 public class Render implements Component {
 
-    private Shape shape;
     private Color color;
 
     public Render(Shape shape, Color color) {
-        this.shape = shape;
         this.color = color;
-    }
-
-    public Shape getShape() {
-        return shape;
-    }
-
-    public void setShape(Shape shape) {
-        this.shape = shape;
     }
 
     public Color getColor() {
