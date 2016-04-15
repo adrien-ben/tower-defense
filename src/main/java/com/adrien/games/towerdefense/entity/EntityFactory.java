74e042385315706fd5550af4743daf3775929472
Adrien Bennadji <adrien-ben@users.noreply.github.com>
2016-04-15 16:06:32 +0200

    Fix EntityFactory

--- src/main/java/com/adrien/games/towerdefense/entity/EntityFactory.java
+++ src/main/java/com/adrien/games/towerdefense/entity/EntityFactory.java
@@ -5,9 +5,9 @@ import com.adrien.games.towerdefense.component.*;
 import com.badlogic.ashley.core.Component;
 import com.badlogic.ashley.core.Entity;
 
-import java.util.List;
 import java.awt.*;
 import java.awt.geom.Ellipse2D;
+import java.util.List;
 
 /**
  * Factory responsible of creating entities.
@@ -35,7 +35,7 @@ public class EntityFactory {
                 new Body(size),
                 new Tracker(range, null),
                 new Shoot(rate, damage, range),
-                new Render(new Ellipse2D.Float(0, 0, size, size), Color.BLUE));
+                new Render(Color.BLUE));
     }
 
     public static Entity createBullet(Vector2 position, float size, float speed, int damage, int health, Entity target) {
@@ -47,7 +47,7 @@ public class EntityFactory {
                 new Health(health),
                 new Damage(damage),
                 new Tracker(0, target),
-                new Render(new Ellipse2D.Float(0, 0, size, size), Color.RED));
+                new Render(Color.RED));
     }
 
     public static Entity createMinion(Vector2 position, float size, float speed, List<Vector2> path, int health) {
@@ -58,7 +58,7 @@ public class EntityFactory {
                 new Path(0, path),
                 new Collision(MINION_GROUP),
                 new Health(health),
-                new Render(new Ellipse2D.Float(0, 0, size, size), Color.YELLOW));
+                new Render(Color.YELLOW));
     }
 
     public static Entity createSpawner(Vector2 position, float size, float rate) {
@@ -66,7 +66,7 @@ public class EntityFactory {
                 new Position(position),
                 new Body(size),
                 new Spawner(rate),
-                new Render(new Ellipse2D.Float(0, 0, size, size), Color.GRAY));
+                new Render(Color.GRAY));
     }
 
 }
